b451638f1767ca826ff029c81b833ef11f20aace
José Barboza <barbozag@sheridancollege.ca>
2023-05-16 10:27:23 -0400

    Reverting last comment

--- src/studentlist23tuem/Studentlist23tuem.java
+++ src/studentlist23tuem/Studentlist23tuem.java
@@ -13,7 +13,6 @@ import java.util.Scanner;
 public class Studentlist23tuem {
 
     //Hellos this is commernt
-    //Hello this is commet for rollback
     
     public static void main(String[] args) {
         
