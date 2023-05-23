f5e88dde11360bcad5a40f245b0cda10c1f45935
José Barboza <barbozag@sheridancollege.ca>
2023-05-23 10:05:31 -0400

    Added Part Time Student Class

---
+++ src/studentlist23tuem/PartTimeStudent.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist23tuem;
+
+/**
+ *
+ * @author Jose
+ */
+public class PartTimeStudent extends Student {
+    
+    //Hellow Part Time Students
+
+    public PartTimeStudent(String sname, String sID) {
+        super(sname, sID);
+    }
+    
+    //Hellow Part Time Students
+}
